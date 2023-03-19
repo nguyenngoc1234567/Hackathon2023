85910da1fbbc94761a6974642dbfc9f0607a54e8
nguyenngoc1234567 <ngoc2503pp@gmail.com>
2023-03-19 13:09:55 +0700

    lam viec voi route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [Controller::class, 'index'])->name('home');
 Route::get('/details', [Controller::class, 'detail'])->name('location.detail');
 
+
